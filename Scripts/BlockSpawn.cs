17321c66ad23096c09960eb90ae8396836dd7113
Raj Pathare <rajgpathare@gmail.com>
2019-01-26 09:42:55 +0530

    Add files via upload

---
+++ Scripts/BlockSpawn.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class BlockSpawn : MonoBehaviour {
+
+	public Transform[] spawnPoints;
+
+	public GameObject blockPrefab;
+
+
+	private float timeToSpawn = 0.5f;
+
+	public float timeBetweenW = 0.001f;
+
+	private Rigidbody rb1;
+
+
+	void Start() {
+
+		rb1 = GetComponent<Rigidbody>();
+
+	}
+
+	// Use this for initialization
+	void Update () 
+
+	{
+
+	 	if(Time.time >= timeToSpawn)
+	 	{
+	 		SpawnBlocks();
+	 		timeToSpawn = Time.time + timeBetweenW;
+	 	}
+
+	 	//rb1.AddForce(0,0,6f);
+	 	blockSpeed();
+
+	 	}
+		
+
+
+	void blockSpeed () {
+
+		var pos1 = transform.position;
+
+		pos1.z = pos1.z + 0.2f;
+		transform.position = pos1;
+		
+
+	}
+	
+	// Update is called once per frame
+	void SpawnBlocks () 
+
+	{
+
+		int randomIndex = Random.Range(0, spawnPoints.Length);
+
+		for (int i =0; i < spawnPoints.Length ; i++) 
+		{
+
+			if(randomIndex != i)
+
+			{
+				Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+			}
+
+			
+
+		}
+		
+	}
+
+
+
+}
