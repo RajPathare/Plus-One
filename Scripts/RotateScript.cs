290ac81366a260c959fa16411eceec7075c37517
Raj Pathare <rajgpathare@gmail.com>
2019-02-02 20:24:08 +0530

    Add files via upload

---
+++ Scripts/RotateScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+	public float speed = 15f;
+	public playerColl coll;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(speed, 0, 0);
+
+
+		}
+		
+	}
+
